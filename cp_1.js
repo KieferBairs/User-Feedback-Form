ede7c73ddd3f402c567c8a9a1e1ff5fa015c1c5a
Kiefer Bairstow <bairstow@usf.edu>
2025-10-15 23:55:12 -0400

    Began JS element

---
+++ cp_1.js
@@ -0,0 +1,6 @@
+const page = document.getElementById("page");
+const wrap = document.getElementById("form-container");
+const form = document.getElementById("feedback-form");
+const list = document.getElementById("feedback-display");
+const tip = document.getElementById("tooltip");
+
