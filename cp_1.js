c03fd791d7024c209483a835f83aa83878ddb99e
Kiefer Bairstow <bairstow@usf.edu>
2025-10-16 00:01:59 -0400

    Created event listener for charecter count

--- cp_1.js
+++ cp_1.js
@@ -4,3 +4,11 @@ const form = document.getElementById("feedback-form");
 const list = document.getElementById("feedback-display");
 const tip = document.getElementById("tooltip");
 
+// Character Count
+form.addEventListener('input', (e) => {
+    const t =e.target;
+    if (!t.matches('#name, #email, #comments')) return;
+    const count =t.closest('.field').querySelector('count');
+    if (t.id === 'comments') count.textContent = `${t.value.length}/${t.maxLength}`;
+    else count.textContent = t.value.length;
+});
